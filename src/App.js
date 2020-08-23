2ea921b312bb0c801be4bdae890999d523d1fe48
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-23 15:09:56 -0500

    Basic home page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
@@ -8,9 +9,7 @@ function App() {
     <div>
       <NavBar />
       <body>
-        <h1>
-          This is the Home Page
-        </h1>
+        <Home />
       </body>
       <Footer />
     </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home-body">
+            <h1 className="welcome">Welcome to <span className="company-name2">Macro Meals</span></h1>
+            <p className="home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit sed vulputate mi sit amet mauris. Senectus et netus et malesuada fames ac turpis egestas. Facilisis volutpat est velit egestas dui id ornare. Vitae suscipit tellus mauris a diam maecenas sed enim ut. Nisi quis eleifend quam adipiscing. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu. Eu ultrices vitae auctor eu. Ut tortor pretium viverra suspendisse potenti nullam. Vitae tortor condimentum lacinia quis vel. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.</p>
+            <p className="home-description">Ac orci phasellus egestas tellus rutrum. Morbi tristique senectus et netus et malesuada fames ac. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Elit ut aliquam purus sit amet luctus venenatis lectus. Orci eu lobortis elementum nibh tellus molestie nunc. Felis eget velit aliquet sagittis id consectetur purus. Sodales ut eu sem integer. Dolor magna eget est lorem ipsum dolor. Convallis tellus id interdum velit. Tellus mauris a diam maecenas sed. Pharetra et ultrices neque ornare aenean euismod elementum.</p>
+        </div>
+    )
+}
+
+export default Home;
