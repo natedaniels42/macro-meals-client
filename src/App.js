0995fdff4038d2eae3ff1257c1a89e9d29455411
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-23 15:25:08 -0500

    Added config folder and routes and connected to index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Routes from './config/routes';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
@@ -9,7 +11,7 @@ function App() {
     <div>
       <NavBar />
       <body>
-        <Home />
+        <Routes />
       </body>
       <Footer />
     </div>
